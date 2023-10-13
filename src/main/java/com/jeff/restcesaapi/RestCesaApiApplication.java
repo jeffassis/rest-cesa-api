765e1ec4b1eb7e1d65a61376dd22225fe6d3ed02
jeffassis <cmdtjeff@gmail.com>
2023-10-13 13:05:52 -0300

    first commit

---
+++ src/main/java/com/jeff/restcesaapi/RestCesaApiApplication.java
@@ -0,0 +1,13 @@
+package com.jeff.restcesaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestCesaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestCesaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeff/restcesaapi/RestCesaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeff.restcesaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestCesaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
